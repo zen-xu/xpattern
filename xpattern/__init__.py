bc0f99a4ea7c31253ad985e2b10110c85fd5b70f
ZhengYu, Xu <zen-xu@outlook.com>
2023-07-26 22:15:20 +0800

    fix: correct ruff errors

--- xpattern/__init__.py
+++ xpattern/__init__.py
@@ -1,13 +1,13 @@
 __version__ = "0.5.0"
 
-from pampy import ANY
-from pampy import HEAD
-from pampy import REST
-from pampy import TAIL
-from pampy import MatchError
-from pampy import _
+from pampy import ANY as ANY
+from pampy import HEAD as HEAD
+from pampy import REST as REST
+from pampy import TAIL as TAIL
+from pampy import MatchError as MatchError
+from pampy import _ as _
 
-from ._xobject import X
-from ._xobject import xfunction
-from ._xpattern import caseof
-from ._xpattern import m
+from ._xobject import X as X
+from ._xobject import xfunction as xfunction
+from ._xpattern import caseof as caseof
+from ._xpattern import m as m
